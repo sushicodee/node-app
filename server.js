c31b920e73bfc8ca06cce09a78ad436c4a793a3b
sushicodee <ssushant.sss@gmail.com>
2020-11-02 22:33:28 +0545

    enable cors

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ const { PORT } = config;
 // }
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 require('./socket')(app);
 //load middlewares
 const aunthenticate = require('./middlewares/authenticate');
