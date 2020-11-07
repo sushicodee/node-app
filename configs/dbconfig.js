abf8078f088f77928bf7b5a0c54d943ec9e07e8f
sushicodee <ssushant.sss@gmail.com>
2020-11-07 15:47:12 +0545

    connection uri updated

--- configs/dbconfig.js
+++ configs/dbconfig.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 if(process.env.NODE_ENV === "development"){
-  connectionUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fdomo.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
+  connectionUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fdomo.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true`
 }
 console.log(process.env.NODE_ENV)
 module.exports = {
